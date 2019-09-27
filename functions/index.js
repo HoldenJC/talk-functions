4b6bf039010172ff69244b1a6d8ca1aa7b171327
HoldenJC <holdenj_c@yahoo.com>
2019-09-27 16:54:35 -0700

    update one line on index.js and create admin.js file

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const { db } = require('./util/admin');
+
 const { getAllTalks } = require('./handlers/talks');
 
 // route to 'GET' all 'talks'
