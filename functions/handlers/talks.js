62294cdf3360aaa55c57f034e06059de947ccf37
Holden John Clark <Holden@Lins-MacBook-Pro.local>
2019-09-30 09:17:02 -0700

    add users.js, fbAuth.js, validators.js, and update talks.js, admin.js, index.js

--- functions/handlers/talks.js
+++ functions/handlers/talks.js
@@ -17,8 +17,35 @@ exports.getAllTalks = (req, res) => {
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).json({
-                error: err.code
-            });
+            res.status(500).json({ error: err.code });
         });
 };
+
+exports.postTalk = (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({
+            body: 'Body text must not be empty'
+        });
+    }
+
+    const newTalk = {
+        body: req.body.body,
+        userHandle: req.user.handle,
+        createdAt: new Date().toISOString()
+    };
+
+    db
+        .collection('talks')
+        .add(newTalk)
+        .then((doc) => {
+            const resTalk = newTalk;
+            resTalk.talkId = doc.id;
+            res.json(resTalk);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: 'something went wrong'
+            });
+            console.error(err);
+        });
+}
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,22 @@
 const functions = require('firebase-functions');
 const app = require('express')();
+const FBAuth = require('./util/fbAuth');
 
 const { db } = require('./util/admin');
 
-const { getAllTalks } = require('./handlers/talks');
+const { getAllTalks, postTalk } = require('./handlers/talks');
+const { signup, login } = require('./handlers/users');
 
-// route to 'GET' all 'talks'
+// GET all 'talks' route
 app.get('/talks', getAllTalks);
+
+// POST a 'talk' route
+app.post('/talk', FBAuth, postTalk);
+
+// SIGNUP route
+app.post('/signup', signup);
+
+// LOGIN route
+app.post('/login', login);
 
 exports.api = functions.https.onRequest(app);
